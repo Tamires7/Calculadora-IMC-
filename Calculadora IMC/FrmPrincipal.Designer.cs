097964a3bf76c8a1bc932236beb73f5236904cac
Windwos 10 <Windwos 10@DESKTOP-K9G0L9G>
2022-03-16 10:14:26 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora IMC/FrmPrincipal.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Calculadora_IMC
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.lbPeso = new System.Windows.Forms.Label();
+            this.lbAltura = new System.Windows.Forms.Label();
+            this.lbResultadoIMC = new System.Windows.Forms.Label();
+            this.lbClassificacao = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbPeso
+            // 
+            this.lbPeso.AutoSize = true;
+            this.lbPeso.Font = new System.Drawing.Font("Impact", 8.25F);
+            this.lbPeso.Location = new System.Drawing.Point(14, 41);
+            this.lbPeso.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbPeso.Name = "lbPeso";
+            this.lbPeso.Size = new System.Drawing.Size(78, 15);
+            this.lbPeso.TabIndex = 0;
+            this.lbPeso.Text = "Informe o peso:";
+            // 
+            // lbAltura
+            // 
+            this.lbAltura.AutoSize = true;
+            this.lbAltura.Font = new System.Drawing.Font("Impact", 8.25F);
+            this.lbAltura.Location = new System.Drawing.Point(14, 85);
+            this.lbAltura.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbAltura.Name = "lbAltura";
+            this.lbAltura.Size = new System.Drawing.Size(83, 15);
+            this.lbAltura.TabIndex = 1;
+            this.lbAltura.Text = "Informe a altura:";
+            // 
+            // lbResultadoIMC
+            // 
+            this.lbResultadoIMC.AutoSize = true;
+            this.lbResultadoIMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultadoIMC.Location = new System.Drawing.Point(6, 257);
+            this.lbResultadoIMC.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbResultadoIMC.Name = "lbResultadoIMC";
+            this.lbResultadoIMC.Size = new System.Drawing.Size(31, 15);
+            this.lbResultadoIMC.TabIndex = 2;
+            this.lbResultadoIMC.Text = "------";
+            this.lbResultadoIMC.Visible = false;
+            // 
+            // lbClassificacao
+            // 
+            this.lbClassificacao.AutoSize = true;
+            this.lbClassificacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbClassificacao.Location = new System.Drawing.Point(6, 294);
+            this.lbClassificacao.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbClassificacao.Name = "lbClassificacao";
+            this.lbClassificacao.Size = new System.Drawing.Size(31, 15);
+            this.lbClassificacao.TabIndex = 3;
+            this.lbClassificacao.Text = "------";
+            this.lbClassificacao.Visible = false;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Font = new System.Drawing.Font("Impact", 8.25F);
+            this.txtPeso.Location = new System.Drawing.Point(114, 35);
+            this.txtPeso.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(103, 21);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Font = new System.Drawing.Font("Impact", 8.25F);
+            this.txtAltura.Location = new System.Drawing.Point(114, 79);
+            this.txtAltura.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(103, 21);
+            this.txtAltura.TabIndex = 5;
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btCalcular.Font = new System.Drawing.Font("Impact", 8.25F);
+            this.btCalcular.Location = new System.Drawing.Point(55, 155);
+            this.btCalcular.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(175, 46);
+            this.btCalcular.TabIndex = 6;
+            this.btCalcular.Text = "Calcular o IMC";
+            this.btCalcular.UseVisualStyleBackColor = false;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 360);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lbClassificacao);
+            this.Controls.Add(this.lbResultadoIMC);
+            this.Controls.Add(this.lbAltura);
+            this.Controls.Add(this.lbPeso);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.Text = "Calculadora IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbPeso;
+        private System.Windows.Forms.Label lbAltura;
+        private System.Windows.Forms.Label lbResultadoIMC;
+        private System.Windows.Forms.Label lbClassificacao;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btCalcular;
+    }
+}
+
---
+++ Calculadora IMC/FrmPrincipal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_IMC
+{
+    public class Imc
+    {
+        private float peso;
+        private float altura;
+        
+        public float Altura
+        {
+            get => altura;
+            set => altura = value; 
+        }
+
+        public float Peso
+        {
+            get => peso;
+            set => peso = value;
+        }
+
+        public float calculadoraImc() 
+        {
+            float imc = peso / (altura * altura);
+            return imc;
+        }    
+    }
+}
